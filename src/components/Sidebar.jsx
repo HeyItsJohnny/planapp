07631963fc6d8eb959b6bcd963d7d58e8114cb83
Jonathan Laroco <jonathan.laroco@gmail.com>
2023-08-24 16:41:22 -0700

    stuff

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -47,7 +47,7 @@ const Sidebar = () => {
               onClick={handleCloseSizeBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Plan App</span>
+              <SiShopware /> <span>Day Trip</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
